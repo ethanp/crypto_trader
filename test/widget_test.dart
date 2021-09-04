abfd16e350b8186c887d00f70fd3eb43c63eb781
Ethan Petuchowski <etahnp@gmail.com>
2021-09-04 02:20:05 -0400

    Make the widget_test.dart pass

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,6 @@
 
 import 'package:crypto_trader/data/data_sources.dart';
 import 'package:crypto_trader/widgets/app_dependencies.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
@@ -15,16 +14,10 @@ void main() {
     // Build our app and trigger a frame.
     await tester.pumpWidget(AppDependencies(prices: Prices.fake()));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('Current Bitcoin price:'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // TODO Tap the Coin icon and trigger a frame once it's implemented
+    // await tester.tap(find.byIcon(Icons.monetization_on_outlined));
+    // await tester.pump();
   });
 }
