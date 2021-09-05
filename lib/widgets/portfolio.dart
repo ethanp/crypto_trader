610d60e69a519f08e609686a1346178970ed6d37
Ethan Petuchowski <etahnp@gmail.com>
2021-09-05 14:06:25 -0400

    Delete pie chart, improve data table (liked table better than chart)

--- lib/widgets/portfolio.dart
+++ lib/widgets/portfolio.dart
@@ -1,6 +1,5 @@
 import 'package:crypto_trader/data/data_sources.dart';
 import 'package:crypto_trader/data_model.dart';
-import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -13,87 +12,56 @@ class Portfolio extends StatelessWidget {
       builder: (BuildContext ctx, AsyncSnapshot<List<Holding>> snapshot) {
         return Column(children: [
           _title(),
-          Flexible(child: _chart(snapshot.data)),
+          Flexible(child: _table(snapshot.data)),
           _portfolioTotal(snapshot.data),
         ]);
       },
     );
   }
 
-  Widget _chart(List<Holding>? snapshot) {
-    return Row(children: [
-      Flexible(child: _pieChart(snapshot)),
-      SizedBox(height: 10),
-      Flexible(child: _legend(snapshot)),
-    ]);
-  }
-
-  Widget _pieChart(List<Holding>? snapshot) {
-    return PieChart(PieChartData(
-      sections: snapshot?.map((holding) => _section(holding)).toList(),
-    ));
-  }
-
-  PieChartSectionData _section(Holding holding) {
-    return PieChartSectionData(
-      value: holding.dollarValue.amt,
-      title: '',
-      badgeWidget: _sectionLabel(holding),
-      color: holding.currency.chartColor,
-    );
-  }
-
-  Widget _sectionLabel(Holding holding) {
-    final Widget currencyName = Text(
-      holding.currency.name,
-      style: TextStyle(
-        fontSize: 14,
-        fontWeight: FontWeight.bold,
-      ),
-    );
-    final Widget currencyValue = Text(holding.dollarValue.toString());
-    return SizedBox(
-      height: 40,
-      child: Column(children: [currencyName, currencyValue]),
-    );
-  }
-
-  Widget _legend(List<Holding>? snapshot) {
+  Widget _table(List<Holding>? snapshot) {
     return ListView.builder(
-      itemBuilder: (ctx, idx) => Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            height: 10,
-            width: 10,
-            decoration: BoxDecoration(
-              color: currencies[idx].chartColor,
-              shape: BoxShape.circle,
-            ),
-          ),
-          Text(currencies[idx].name),
-          Text(snapshot?[idx].dollarValue.toString() ?? "Loading"),
-        ]
-            .map(
-              (w) => Flexible(
-                child: Padding(
-                  padding: const EdgeInsets.all(2),
-                  child: w,
-                ),
-              ),
-            )
-            .toList(),
-      ),
+      itemBuilder: (ctx, idx) {
+        final double total = (_totalValue(snapshot)?.amt ?? 1);
+        final double thisOne = (snapshot?[idx].dollarValue.amt ?? 0);
+        final int percentage = (thisOne / total * 100).round();
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _colorRef(idx),
+            Text(currencies[idx].name),
+            Text(snapshot?[idx].dollarValue.toString() ?? "Loading"),
+            Text('$percentage%')
+          ].map(_addPadding).toList(),
+        );
+      },
       itemCount: currencies.length,
     );
   }
 
+  Widget _colorRef(int idx) {
+    return Container(
+      height: 10,
+      width: 10,
+      decoration: BoxDecoration(
+        color: currencies[idx].chartColor,
+        shape: BoxShape.circle,
+      ),
+    );
+  }
+
+  Widget _addPadding(w) => Padding(
+        padding: const EdgeInsets.all(2),
+        child: w,
+      );
+
   Widget _title() => Text('Portfolio');
 
   Widget _portfolioTotal(List<Holding>? snapshot) {
-    final String? total = snapshot
-        ?.fold<Dollars>(Dollars(0), (acc, e) => acc + e.dollarValue.amt)
-        .toString();
+    final String? total = _totalValue(snapshot).toString();
     return Text('Total: ' + (total ?? "Loading"));
   }
+
+  Dollars? _totalValue(List<Holding>? snapshot) =>
+      snapshot?.fold<Dollars>(Dollars(0), (acc, e) => acc + e.dollarValue.amt);
 }
