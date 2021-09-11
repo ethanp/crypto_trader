a83486c52b28ccb6f63256bf1aa21cfbbdbfb7e8
Ethan Petuchowski <etahnp@gmail.com>
2021-09-11 14:50:44 -0400

    Minor refactor

--- lib/widgets/spend_buttons.dart
+++ lib/widgets/spend_buttons.dart
@@ -11,29 +11,34 @@ class SpendButtons extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        ElevatedButton(
-          style: ElevatedButton.styleFrom(primary: Colors.green),
-          onPressed: () {
-            Trader.api.deposit(_amount);
-            context.read<Notifier>().triggerUiRebuild();
-          },
-          child: Text('Transfer $_amount from Schwab'),
-        ),
-        ElevatedButton(
-          onPressed: () {
-            Trader.api.spend(_amount);
-            context.read<Notifier>().triggerUiRebuild();
-          },
-          child: FutureBuilder<Holdings>(
-            future: Trader.api.getMyHoldings(),
-            builder: (ctx, snapshot) {
-              final currency = snapshot.data?.shortest.currency.name;
-              return Text('Spend $_amount on ${currency ?? '(Loading...)'}');
-            },
-          ),
-        ),
-      ],
+      children: [_depositButton(context), _spendButton(context)],
+    );
+  }
+
+  Widget _depositButton(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(primary: Colors.green),
+      onPressed: () {
+        Trader.api.deposit(_amount);
+        context.read<Notifier>().triggerUiRebuild();
+      },
+      child: Text('Deposit $_amount from Schwab'),
+    );
+  }
+
+  Widget _spendButton(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        Trader.api.spend(_amount);
+        context.read<Notifier>().triggerUiRebuild();
+      },
+      child: FutureBuilder<Holdings>(
+        future: Trader.api.getMyHoldings(),
+        builder: (ctx, snapshot) {
+          final currency = snapshot.data?.shortest.currency.name;
+          return Text('Spend $_amount on ${currency ?? '(Loading...)'}');
+        },
+      ),
     );
   }
 }
