3c19890027e0cea1ef3699b05cc8076b2c55cd91
Ethan Petuchowski <etahnp@gmail.com>
2021-10-07 22:40:19 -0400

    Rearrange code

--- lib/screens/total_holdings.dart
+++ lib/screens/total_holdings.dart
@@ -4,55 +4,35 @@ import 'package:flutter/material.dart';
 
 class TotalHoldings extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return Card(
+  Widget build(BuildContext context) => Card(
       margin: EdgeInsets.zero,
       elevation: 15,
-      child: _addGradient(
-        child: Padding(
-          padding: const EdgeInsets.only(bottom: 20, left: 25),
-          child: WithHoldings(
-            builder: (holdings) => Column(
-              children: [
-                _cryptoHoldings(holdings),
-                _cryptoEarnings(holdings),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
+      child: Container(
+          decoration: _gradient,
+          child: Padding(
+              padding: const EdgeInsets.only(bottom: 20, left: 25),
+              child: Column(children: [
+                _cryptoHoldings(),
+                _cryptoEarnings(),
+              ]))));
 
-  Widget _addGradient({required Widget child}) {
-    return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
+  BoxDecoration get _gradient => BoxDecoration(
+      gradient: LinearGradient(
           begin: Alignment.topRight,
           end: Alignment.bottomRight,
-          colors: [_Style.appBarGradientTop, _Style.appBarGradientBottom],
-        ),
-      ),
-      child: child,
-    );
-  }
+          colors: [_Style.appBarGradientTop, _Style.appBarGradientBottom]));
 
-  Widget _cryptoHoldings(Holdings? holdings) {
-    return _element(
-      title: 'Crypto holdings',
-      value: holdings?.totalCryptoValue.toString(),
-    );
-  }
+  Widget _cryptoHoldings() => WithHoldings(
+      builder: (holdings) => _element(
+          title: 'Crypto holdings',
+          value: holdings?.totalCryptoValue.toString()));
 
-  Widget _cryptoEarnings(Holdings? holdings) {
-    return WithEarnings(
-      builder: (Dollars? earnings) => _element(
-        title: 'Crypto earnings',
-        value: earnings?.toString(),
-        percent: _percent(holdings, earnings),
-      ),
-    );
-  }
+  Widget _cryptoEarnings() => WithHoldings(
+      builder: (holdings) => WithEarnings(
+          builder: (Dollars? earnings) => _element(
+              title: 'Crypto earnings',
+              value: earnings?.toString(),
+              percent: _percent(holdings, earnings))));
 
   double _percent(Holdings? holdings, Dollars? earnings) {
     // Default 1 instead of 0 so we don't get a NaN during division below.
@@ -65,20 +45,20 @@ class TotalHoldings extends StatelessWidget {
     required String title,
     required String? value,
     double? percent,
-  }) {
-    return Padding(
-      padding: const EdgeInsets.all(5),
-      child: Row(children: [
-        Text('$title: ', style: _Style.labelStyle),
-        Text('${value ?? 'Loading...'}', style: _Style.amountStyle),
-        if (percent != null)
-          Padding(
-            padding: const EdgeInsets.only(left: 14),
-            child: Text('+${percent.toInt()}%', style: _Style.percentStyle),
-          ),
-      ]),
-    );
-  }
+  }) =>
+      Padding(
+          padding: const EdgeInsets.all(5),
+          child: Row(children: [
+            Text('$title: ', style: _Style.labelStyle),
+            Text('${value ?? 'Loading...'}', style: _Style.amountStyle),
+            if (percent != null)
+              Padding(
+                  padding: const EdgeInsets.only(left: 14),
+                  child: Text(
+                    '+${percent.toInt()}%',
+                    style: _Style.percentStyle,
+                  ))
+          ]));
 }
 
 class _Style {
