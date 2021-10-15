4433f18c77fe106b2c619d8ef37035413b0c4bd8
Ethan Petuchowski <etahnp@gmail.com>
2021-10-15 13:34:54 -0400

    minor cleanups

--- lib/data/controller/brokerage.dart
+++ lib/data/controller/brokerage.dart
@@ -5,57 +5,63 @@ import 'package:crypto_trader/import_facade/model.dart';
 import 'package:flutter/material.dart';
 import 'package:synchronized/synchronized.dart';
 
-abstract class Trader extends ChangeNotifier {
+/// Enables trading cryptocurrencies.
+abstract class Brokerage extends ChangeNotifier {
   final HoldingsCache _holdingsCache = HoldingsCache();
 
   final _synchronizer = Lock(reentrant: true);
 
+  /// Earnings = Holdings - Deposits.
   Future<Dollars> getMyEarnings() async {
     final Holdings holdings = await getMyHoldings();
-    final Dollars deposits = await getTotalDeposits();
+    final Dollars deposits = await _getTotalDeposits();
     return holdings.totalValue - deposits;
   }
 
-  Future<Dollars> getTotalDeposits();
+  Future<Dollars> _getTotalDeposits();
 
+  /// Retrieve [Holdings], possibly from cache.
   Future<Holdings> getMyHoldings() =>
       _synchronizer.synchronized(_holdingsCache.get);
 
   @protected
-  Future<String> spendInternal(Holding holding);
+  Future<String> _spendInternal(Holding holding);
 
   @protected
-  Future<String> depositInternal(Dollars dollars);
+  Future<String> _depositInternal(Dollars dollars);
 
+  /// Trade USD for the "shortest" currency. Ie. the one where we have the
+  /// largest deficit compared to its portfolio allocation.
   Future<String> spend(Dollars dollars) => _synchronizer.synchronized(() async {
         print('Spending $dollars');
-        return spendInternal(Holding(
+        return _spendInternal(Holding(
           currency: (await getMyHoldings()).shortest.currency,
           dollarValue: dollars,
         ));
       });
 
+  /// Deposit [dollars] into brokerage account from linked checking account.
   Future<String> deposit(Dollars dollars) =>
       _synchronizer.synchronized(() async {
         print('Depositing $dollars');
-        return depositInternal(dollars);
+        return _depositInternal(dollars);
       });
 
-  Future<void> invalidateHoldings() =>
+  Future<void> _invalidateHoldings() =>
       _synchronizer.synchronized(_holdingsCache.invalidate);
 
   /// Retrieve [Holdings] from remote source and cache new response.
   Future<Holdings> forceRefreshHoldings() async {
-    await invalidateHoldings();
+    await _invalidateHoldings();
     return getMyHoldings();
   }
 }
 
-class FakeTrader extends Trader {
+class FakeBrokerage extends Brokerage {
   Dollars _spending = Dollars(10);
 
   @override
-  Future<String> spendInternal(Holding holding) async {
+  Future<String> _spendInternal(Holding holding) async {
     print('Fake-buying ${holding.asPurchaseStr}');
     final holdings = await getMyHoldings();
     // Seems ok to violate the "dot-dot principle" here since it's a fake :)
@@ -70,7 +76,7 @@ class FakeTrader extends Trader {
   }
 
   @override
-  Future<String> depositInternal(Dollars deposit) async {
+  Future<String> _depositInternal(Dollars deposit) async {
     print('Fake-transferring $deposit from Schwab');
     final holdings = await getMyHoldings();
     holdings.dollarsOf(Currencies.dollars).amt += deposit.amt;
@@ -78,29 +84,30 @@ class FakeTrader extends Trader {
   }
 
   @override
-  Future<Dollars> getTotalDeposits() => Future.value(_spending);
+  Future<Dollars> _getTotalDeposits() => Future.value(_spending);
 }
 
-class CoinbaseProTrader extends Trader {
+/// Trade cryptocurrencies on Coinbase Pro, via the [CoinbaseApi].
+class CoinbaseProBrokerage extends Brokerage {
   /// https://docs.pro.coinbase.com/?ruby#place-a-new-order
   @override
-  Future<String> spendInternal(Holding order) async {
+  Future<String> _spendInternal(Holding order) async {
     final String orderResponse = await CoinbaseApi().marketOrder(order);
     print('Order: $orderResponse');
     final dynamic decoded = jsonDecode(orderResponse);
-    await invalidateHoldings();
+    await _invalidateHoldings();
     return decoded['id'] as String;
   }
 
   @override
-  Future<String> depositInternal(Dollars dollars) async {
+  Future<String> _depositInternal(Dollars dollars) async {
     final String depositResponse = await CoinbaseApi().deposit(dollars);
     print('Deposit: $depositResponse');
     final dynamic decoded = jsonDecode(depositResponse);
-    await invalidateHoldings();
+    await _invalidateHoldings();
     return decoded['id'] as String;
   }
 
   @override
-  Future<Dollars> getTotalDeposits() => CoinbaseApi().totalDeposits();
+  Future<Dollars> _getTotalDeposits() => CoinbaseApi().totalDeposits();
 }
--- lib/data/controller/environment.dart
+++ lib/data/controller/environment.dart
@@ -2,6 +2,6 @@ import 'package:crypto_trader/import_facade/controller.dart';
 
 class Environment {
   static const bool fake = false;
-  static Trader trader = fake ? FakeTrader() : CoinbaseProTrader();
+  static Brokerage trader = fake ? FakeBrokerage() : CoinbaseProBrokerage();
   static Prices prices = fake ? FakePrices() : CoinbaseProPrices();
 }
