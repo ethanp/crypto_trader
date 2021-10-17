bb8fde898c67e02b78a17220e2fa4c851d3d7301
Ethan Petuchowski <etahnp@gmail.com>
2021-10-17 00:19:26 -0400

    Remove redundant row

--- lib/widgets/deposit_row.dart
+++ lib/widgets/deposit_row.dart
@@ -47,26 +47,22 @@ class DepositDropdown extends StatelessWidget {
     return SizedBox(
       width: 80,
       height: 55,
-      child: Column(
-        children: [
-          DropdownButtonFormField<int>(
-            value: dropdownValue.wrappedInt,
-            decoration: InputDecoration(
-              filled: true,
-              fillColor: Colors.grey[800],
-            ),
-            icon: const Icon(Icons.attach_money),
-            enableFeedback: true,
-            iconSize: 16,
-            onChanged: (int? newValue) => dropdownChanged(newValue!),
-            items: [
-              for (final dropdownValue in [10, 20, 50])
-                DropdownMenuItem(
-                  value: dropdownValue,
-                  child: Text(dropdownValue.toString()),
-                )
-            ],
-          ),
+      child: DropdownButtonFormField<int>(
+        value: dropdownValue.wrappedInt,
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: Colors.grey[800],
+        ),
+        icon: const Icon(Icons.attach_money),
+        enableFeedback: true,
+        iconSize: 16,
+        onChanged: (int? newValue) => dropdownChanged(newValue!),
+        items: [
+          for (final dropdownValue in [10, 20, 50])
+            DropdownMenuItem(
+              value: dropdownValue,
+              child: Text(dropdownValue.toString()),
+            )
         ],
       ),
     );
