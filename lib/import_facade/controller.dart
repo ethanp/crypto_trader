fc9ce4f4780176cffa2211fc05a92aecfa3861b7
Ethan Petuchowski <etahnp@gmail.com>
2021-10-17 15:52:45 -0400

    Move Candle into its own file

--- lib/import_facade/controller.dart
+++ lib/import_facade/controller.dart
@@ -1,5 +1,6 @@
 export '../data/access/coinbase_api.dart';
 export '../data/controller/brokerage.dart';
 export '../data/controller/cached_value.dart';
+export '../data/controller/candle.dart';
 export '../data/controller/environment.dart';
 export '../data/controller/price_source.dart';
