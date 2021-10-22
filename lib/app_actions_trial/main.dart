009a6f4db9aba300ba145eb6b8c5d6a094e1ec71
Ethan Petuchowski <etahnp@gmail.com>
2021-10-22 17:00:28 -0400

    app actions trial: Inject executor

--- lib/app_actions_trial/main.dart
+++ lib/app_actions_trial/main.dart
@@ -1,24 +1,37 @@
+import 'package:crypto_trader/data/controller/app_actions.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(AppActionsTrialTopLevel());
 
 class AppActionsTrialTopLevel extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'App actions trial',
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              OutlinedButton(onPressed: _onPressed, child: const Text('Start')),
-              const SizedBox(height: 10),
-              const Text('State: Has not started'),
-            ],
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => MultistageActionExecutor())
+      ],
+      builder: (context, _) {
+        context.watch<MultistageActionExecutor>();
+        return MaterialApp(
+          title: 'App actions trial',
+          home: Scaffold(
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  OutlinedButton(
+                    onPressed: _onPressed,
+                    child: const Text('Start'),
+                  ),
+                  const SizedBox(height: 10),
+                  const Text('State: Has not started'),
+                ],
+              ),
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 
